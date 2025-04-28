76a25b76f40e2c4a0eac65c66b38fb61f4464ebf
fakhar054 <45002007+fakhar054@users.noreply.github.com>
2025-04-28 18:26:23 +0500

    First commit

---
+++ src/app/user-profile/page.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./user-profile.css";
+
+export default function page() {
+  return (
+    <section className="user_profile">
+      <div className="container">
+        <h1>user profile</h1>
+      </div>
+    </section>
+  );
+}
